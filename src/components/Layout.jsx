699a53b6ddd97836453e62c33d6db960644dc4d8
AdaHep <adam.hepsever@gmail.com>
2022-04-22 11:46:17 +0200

    Gittus commitutts

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,24 +1,24 @@
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Header.jsx";
 import LoginPage from "./LoginPage.jsx";
 import TestButtons from "./TestButtons.jsx";
-import StartPage from "./StartPage.jsx"
+import StartPage from "./StartPage.jsx";
 import AdminPage from "./AdminPage.jsx";
 import PostsPage from "./PostsPage.jsx";
-import TestPage from "./TestPage.tsx
+import TestPage from "./TestPage.tsx";
+
 function Layout() {
   return (
     <BrowserRouter>
       <Header />
       <div className="layout-div">
-          <TestButtons />
+        <TestButtons />
         <Routes>
-          <Route path="/" element={ <StartPage />}/>
-          <Route path="/login" element={ <LoginPage />}/>
-          <Route path="/admin" element={ <AdminPage />}/>
-          <Route path="/posts" element={ <PostsPage />}/>
-          <Route path="/testpage" element={ <TestPage />}/>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/admin" element={<AdminPage />} />
+          <Route path="/posts" element={<PostsPage />} />
+          <Route path="/testpage" element={<TestPage />} />
         </Routes>
       </div>
     </BrowserRouter>
