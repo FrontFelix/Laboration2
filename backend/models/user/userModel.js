ce272ed841cb2b3ae59d49ba8976b5f498697aee
AdaHep <adam.hepsever@gmail.com>
2022-04-22 11:21:35 +0200

    Merged schemas into models

--- backend/models/user/userModel.js
+++ backend/models/user/userModel.js
@@ -1,5 +1,33 @@
 const mongoose = require("mongoose");
-const userSchema = require('../../schemas/user_schema')
-const userModel = mongoose.model("User", userSchema)
 
-module.exports = userModel
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    userRealName: {
+      type: String,
+      required: true,
+    },
+    userEmail: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    userPassword: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+export default mongoose.model("user", userSchema);
