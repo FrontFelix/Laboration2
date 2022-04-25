109ed64dbf9f01a8a426d9f4d9138a45b466e10a
AdaHep <adam.hepsever@gmail.com>
2022-04-25 11:19:35 +0200

    Skapa go layout för posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,37 +1,22 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
+
 import CardContent from "@mui/material/CardContent";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
 export const PostCard = ({ post }) => {
-  return <div>{post.postTitle}</div>;
+  return (
+    <div class="postContainer">
+      <React.Fragment>
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+            {post.author}
+          </Typography>
+          <Typography variant="h5" component="div">
+            {post.postTitle}
+          </Typography>
+          <Typography variant="body2">{post.content}</Typography>
+        </CardContent>
+      </React.Fragment>
+    </div>
+  );
 };
-
-// const post = (
-//   <React.Fragment>
-//     <CardContent>
-//       <Typography variant="h5" component="div">
-//         ASD
-//       </Typography>
-//       <Typography sx={{ mb: 1.5 }} color="text.secondary">
-//         adjective
-//       </Typography>
-//       <Typography variant="body2">
-//         well meaning and kindly.
-//         <br />
-//         {'"a benevolent smile"'}
-//       </Typography>
-//     </CardContent>
-//   </React.Fragment>
-// );
-
-// export default function OutlinedCard(props) {
-//   return (
-//     <Box sx={{ minWidth: 275 }}>
-//       <Card variant="outlined">{props.postTitle}</Card>
-//     </Box>
-//   );
-// }
--- src/components/PostsPage.jsx
+++ src/components/PostsPage.jsx
@@ -15,10 +15,6 @@ function PostsPage() {
   return (
     <div>
       These are my posts. There are many posts like these, but these are mine.
-      <div className="test-post">
-        <div>Christian</div>
-        <div>Hej, idag åt jag röv och även lite godis.</div>
-      </div>
       {posts.map((post, index) => (
         <div key={index}>
           <PostCard post={post} />
