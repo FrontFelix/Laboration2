97423c10a2f67f7ee47df7b53b07530c2770d957
AdaHep <adam.hepsever@gmail.com>
2022-04-27 10:36:53 +0200

    Fix

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,17 +1,16 @@
 import { useUser } from "../contexts/LoginContext";
 import loginUser from "../pappasTest/loginUser";
 function LoginPage() {
-<<<<<<< Updated upstream
-  // const { FetchLoggedInUser } = useUser()
+  const { fetchUser } = useUser();
   return (
     <div>
-      <form onSubmit={() => console.log('wagwan')} className="login-form">
-=======
-  const { fetchUser } = useUser()
-  return (
-    <div>
-      <form onSubmit={() => {loginUser(); fetchUser()}} className="login-form">
->>>>>>> Stashed changes
+      <form
+        onSubmit={() => {
+          loginUser();
+          fetchUser();
+        }}
+        className="login-form"
+      >
         <label htmlFor="username">Username</label>
         <input id="userUsername" type="text" />
         <label htmlFor="password">Password</label>
